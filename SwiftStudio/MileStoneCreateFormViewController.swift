b1754665b053cfddcbd28b51d7e0608347560aa3
ionic fin <Jyoon@olmangnim.local>
2017-04-19 22:20:17 +0900

    view semi fin.
    need to add navbar

--- SwiftStudio/MileStoneCreateFormViewController.swift
+++ SwiftStudio/MileStoneCreateFormViewController.swift
@@ -13,20 +13,48 @@ class MileStoneCreateFormViewController: FormViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        form +++ Section("Section1")
+        
+        form +++ Section("title")
             <<< TextRow(){ row in
-                row.title = "Text Row"
-                row.placeholder = "Enter text here"
+                row.title = "일정 제목"
+//                row.placeholder = "Enter text here"
             }
-            <<< PhoneRow(){
-                $0.title = "Phone Row"
-                $0.placeholder = "And numbers here"
+            <<< TextRow(){ row in
+                row.title = "일정 설명"
+//                row.placeholder = "Enter text here"
             }
-            +++ Section("Section2")
+            
+            
+            +++ Section("time")
+            <<< TextRow(){ row in
+                row.title = "하루종일"
+                // toggle here            
+            }
             <<< DateRow(){
-                $0.title = "Date Row"
+                $0.title = "시작"
                 $0.value = Date(timeIntervalSinceReferenceDate: 0)
-        }
+            }
+            <<< DateRow(){
+                $0.title = "종료"
+                $0.value = Date(timeIntervalSinceReferenceDate: 0)
+            }
+            <<< TextRow(){ row in
+                row.title = "미리 알림"
+                // toggle here
+            }
+        
+            +++ Section("etc")
+            // map으로 변경
+            <<< TextRow(){ row in
+                row.title = "위치"
+                // map here
+            }
+            <<< TextRow(){ row in
+                row.title = "참석 여부"
+                // toggle here
+            }
+        
+        
         
 //        Section("Section1")
 //            TextRow(){ row in
